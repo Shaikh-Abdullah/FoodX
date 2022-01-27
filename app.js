d0e31673fbc6d11d9221349f0256f41d8c4cf73c
Abdullah <shabdullah685@gmail.com>
2022-01-27 19:50:44 +0530

    add javascript

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ bibimbapDropdown.addEventListener("change", (e) => {
 bibimbapBtn.addEventListener("click", () => {
   localStorage.setItem("bibimbapQty", bibimbapQty);
 });
+
+const thalBtn = document.querySelector("#thalBtn");
+
+let thalQty = 0;
+
+const thalDropdown = document.querySelector("#thalQty");
+
+thalDropdown.addEventListener("change", (e) => {
+  thalQty = e.target.value;
+});
+
+thalBtn.addEventListener("click", () => {
+  localStorage.setItem("thalQty", thalQty);
+});
+
+const burgerBtn = document.querySelector("#burgerBtn");
+
+let burgerQty = 0;
+
+const burgerDropdown = document.querySelector("#burgerQty");
+
+burgerDropdown.addEventListener("change", (e) => {
+  burgerQty = e.target.value;
+});
+
+burgerBtn.addEventListener("click", () => {
+  localStorage.setItem("burgerQty", burgerQty);
+});
