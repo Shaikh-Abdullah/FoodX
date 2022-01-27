2686ef60036958a58947a44fbea41d0b458a9349
Abdullah <shabdullah685@gmail.com>
2022-01-27 19:51:49 +0530

    add JavaScript

--- order.js
+++ order.js
@@ -5,3 +5,11 @@ pizzaQty.innerHTML = localStorage.getItem("pizzaQty");
 const bibimbapQty = document.querySelector("#bibimbapQty");
 
 bibimbapQty.innerHTML = localStorage.getItem("bibimbapQty");
+
+const thalQty = document.querySelector("#thalQty");
+
+thalQty.innerHTML = localStorage.getItem("thalQty");
+
+const burgerQty = document.querySelector("#burgerQty");
+
+burgerQty.innerHTML = localStorage.getItem("burgerQty");
